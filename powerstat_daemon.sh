2eb3a279747c65bc85b28a2c99ddb6021e1aa6dd
stbradle <sebradley13@gmail.com>
2019-11-17 11:24:14 -0800

    Adds powerstat daemon script

---
+++ powerstat_daemon.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Need to add flags and capability for running R scripts and monitoring GPU usage(nvidia-smi)
+
+while getopts 'r:p:g' OPTION; do
+  case "$OPTION" in
+    r)
+      echo "Add R-Script Logic here"
+      ;;
+    p)
+      nohup powerstat -R -z >./power.log &
+      pid=$!
+
+      python3 $OPTARG
+
+      kill $pid	
+      wait $!
+      ;;
+    t)
+      head -n 4 ./power.log
+      tail -n 11 ./power.log
+      ;;
+    g)
+      echo "ADD GPU TRACKING LOGIC HERE"
+      ;;
+    ?)
+      echo "script usage: $(basename $0) [-p python_script.py] [-r r_script.r] [-a]" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
