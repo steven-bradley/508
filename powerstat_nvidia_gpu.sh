a9cb752dcbe17163cc31b08c8d170d31b62cdf6e
hi4a4 <hanson4a4@gmail.com>
2019-11-29 21:15:21 -0800

    In powerstat_nvidia_gpu.sh: Fixed average printing

--- powerstat_nvidia_gpu.sh
+++ powerstat_nvidia_gpu.sh
@@ -2,13 +2,14 @@
 # Note: W = Watt
 # W# = GPU ID #
 # TOTAL# = GPU ID # TOTAL Wattage
+# SMILINE = nvidia-smi output line
+# LOOPS = TOTAL TIMES nvidia-smi was run
 TOTAL1=0
 TOTAL2=0
 W1=0
 W2=0
 SMILINE=0
 LOOPS=1
-
 while true; do
         while read i; do
                 if (($SMILINE == 14)); then
@@ -30,11 +31,11 @@ while true; do
                 ((SMILINE+=1))
         done < <(nvidia-smi)
         TOTAL1=$(($W1 + $TOTAL1))
-	TOTAL2=$(($W2 + $TOTAL2))
-	echo GPU1 AVERAGE WATT > ./gpu.log
-        echo "scale=2; $TOTAL / $LOOPS" | bc >> ./gpu.log
-	echo GPU2 AVERAGE WATT >> ./gpu.log
-        echo "scale=2; $TOTAL / $LOOPS" | bc >> ./gpu.log
+        TOTAL2=$(($W2 + $TOTAL2))
+        echo "GPU1 AVERAGE WATT" > ./gpu.log
+        echo "scale=2; $TOTAL1 / $LOOPS" | bc >> ./gpu.log
+        echo "GPU2 AVERAGE WATT" >> ./gpu.log
+        echo "scale=2; $TOTAL2 / $LOOPS" | bc >> ./gpu.log
         ((LOOPS+=1))
         ((SMILINE=0))
 done
