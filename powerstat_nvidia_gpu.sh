b8cc45e5da5385db612ccd78bf79321a7b2b0a90
hi4a4 <hanson4a4@gmail.com>
2019-11-29 20:51:31 -0800

    Added powerstat for nvidia gpu

---
+++ powerstat_nvidia_gpu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Note: W = Watt
+# W# = GPU ID #
+# TOTAL# = GPU ID # TOTAL Wattage
+TOTAL1=0
+TOTAL2=0
+W1=0
+W2=0
+SMILINE=0
+LOOPS=1
+
+while true; do
+        while read i; do
+                if (($SMILINE == 14)); then
+                        length=${#i}
+                        if ((length == 79)); then
+                                W1=$(($W1 + ${i:20:3}))
+                        else
+                                W1=$(($W1 + ${i:20:4}))
+                        fi
+                fi
+                if (($SMILINE == 17)); then
+                        length=${#i}
+                        if ((length == 79)); then
+                                W2=$(($W2 + ${i:20:3}))
+                        else
+                                W2=$(($W2 + ${i:20:3}))
+                        fi
+                fi
+                ((SMILINE+=1))
+        done < <(nvidia-smi)
+        TOTAL1=$(($W1 + $TOTAL1))
+	TOTAL2=$(($W2 + $TOTAL2))
+	echo GPU1 AVERAGE WATT > ./gpu.log
+        echo "scale=2; $TOTAL / $LOOPS" | bc >> ./gpu.log
+	echo GPU2 AVERAGE WATT >> ./gpu.log
+        echo "scale=2; $TOTAL / $LOOPS" | bc >> ./gpu.log
+        ((LOOPS+=1))
+        ((SMILINE=0))
+done
