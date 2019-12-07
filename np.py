f7e755ead4fcdb29a5c00cabe65faaad3cc85f96
Dmitriy Timokhin <dtimokhi@calpoly.edu>
2019-12-07 12:54:31 -0800

    remove files

--- np.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-np.random.seed(420)
-m = np.random.rand(5000, 5000)
-y = np.random.rand(5000, 1)
-n = np.matmul(m, m)
-n = np.linalg.inv(m)
-b = np.matmul(np.linalg.inv(np.matmul(np.transpose(m), m)), np.matmul(np.transpose(m), y))
-np.linalg.eig(m)
